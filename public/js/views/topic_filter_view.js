ed4931588d534dc7f0dd1953e487b39ede020c32
Metsnugis <inhortte@gmail.com>
2013-12-07 12:21:51 -0600

    laaaaaaaa

--- public/js/views/topic_filter_view.js
+++ public/js/views/topic_filter_view.js
@@ -2,7 +2,7 @@ var TopicFilterView = Ember.View.extend({
   topicFiltersObserver: function() {
     console.log('from view.... topicFilters has changed');
     this.rerender();
-  }.observes('this.controller.topicFilters.[]')
+  }.observes('controller.topicFilters.[]')
 });
 
 module.exports = TopicFilterView;
