3f2d34a926a3dd2403a5d7b19647cb57fb8212ec
Metsnugis <inhortte@gmail.com>
2013-12-08 19:22:32 -0600

    topic filters are added and deleted in the ui

--- public/js/controllers/topic_filter_controller.js
+++ public/js/controllers/topic_filter_controller.js
@@ -1,23 +1,15 @@
 var TopicFilterController = Ember.ObjectController.extend({
-  topicFilters: Ember.A([ ]),
-  areTopicFilters: function() {
-    console.log('topicFilters.length -> ' + this.topicFilters.length);
-    return this.topicFilters.length > 0;
-  }.property('topicFilters'),
-  getTopicFilters: function() {
-    console.log('getTopicFilters....');
-    return this.store.findByIds('topic', this.topicFilters);
-  }.property('topicFilters'),
+  topicFilters: Ember.A(),
   actions: {
     addTopicFilter: function(t) {
-      if(this.topicFilters.indexOf(parseInt(t)) == -1) {
-        this.topicFilters.pushObject(parseInt(t));
+      var topicFilters = this.get('topicFilters');
+      if(!topicFilters.contains(t)) {
+        topicFilters.pushObject(t);
       }
-      // this.topicFilters.add(parseInt(t));
       console.log('topicFilters -> ' + JSON.stringify(this.topicFilters));
     },
     removeTopicFilter: function(t) {
-      this.topicFilters.removeObject(parseInt(t));
+      this.topicFilters.removeObject(t);
       console.log('topicFilters -> ' + JSON.stringify(this.topicFilters));
     }
   }
