f90d20e7df49a58e51887d9957907693d2d30138
Bobbus Mustelidus <inhortte@gmail.com>
2014-01-30 09:41:19 -0600

    individual entry viewing

--- public/js/config/routes.js
+++ public/js/config/routes.js
@@ -2,7 +2,7 @@ var App = require('./app');
 
 App.Router.map(function() {
   this.resource('entries', {path: '/:pagina/entries', queryParams: ['t']});
-  this.resource('entry', {path: '/entry/:entry_id'});
+  this.resource('entry', {path: '/entries/:entry_id'});
   this.resource('entry_count');
   this.resource('topic_filters');
 });
