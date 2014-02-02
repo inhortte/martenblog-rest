f896317f2b817c8bda119b9a2ec1d83af6f42722
Bobbus Mustelidus <inhortte@gmail.com>
2014-02-02 17:50:44 -0600

    fixed next entry / day bug

--- public/js/helpers/format_date.js
+++ public/js/helpers/format_date.js
@@ -5,5 +5,5 @@ Ember.Handlebars.helper('formatDateAbbr', function(epochish) {
   return App.Moment(epochish).format('D MMM HH.mm');
 });
 Ember.Handlebars.helper('formatDay', function(epochish) {
-  return App.Moment(epochish).format('D MMM');
+  return App.Moment(epochish).format('D MMM YYYY');
 });
